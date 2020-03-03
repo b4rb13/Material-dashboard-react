af2f89818b94b78ac8316517803dcfb10d09b0cc
b4rb13 <b4rb13@users.noreply.github.com>
2020-03-04 00:50:13 +0400

    material-dashboard-react

---
+++ src/routes.js
@@ -0,0 +1,45 @@
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import LocationOn from "@material-ui/icons/LocationOn";
+
+// core components/views for Admin layout
+import DashboardPage from "views/Dashboard/Dashboard.js";
+import UserProfile from "views/UserProfile/UserProfile.js";
+import TableList from "views/TableList/TableList.js";
+import Maps from "views/Maps/Maps.js";
+
+// core components/views for RTL layout
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/user",
+    name: "User Profile",
+    icon: Person,
+    component: UserProfile,
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Table List",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/maps",
+    name: "Maps",
+    icon: LocationOn,
+    component: Maps,
+    layout: "/admin"
+  }
+];
+
+export default dashboardRoutes;
